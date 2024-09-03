85a9eb39993ff042fd34132653696141f1682368
Hongbo Li <lihongbo1991@gmail.com>
2024-09-03 03:06:54 +0100

    Fix up jump instructions, and store the actual instruction

--- symrellib.py
+++ symrellib.py
@@ -3,35 +3,44 @@ import re
 import pickle
 
 available_relations = {
-    'call': re.compile(r'^[0-9a-f]+:\s*e8\s[ 0-9a-f]{11}\s*call\s+[0-9a-f]+ <(.+?)>'),
-    'jump': re.compile(r'^[0-9a-f]+:\s*e9\s[ 0-9a-f]{11}\s*jmp\s+[0-9a-f]+ <([^+>]*)>')
+    'call': re.compile(r'^[0-9a-f]+:\s*e8\s[ 0-9a-f]{11}\s*(call)\s+[0-9a-f]+ <(.+?)>'),
+    'jump': re.compile(r'^[0-9a-f]+:\s*[0-9a-f\s]+(jmp|ja|jae|jb|jbe|jl|jle|jg|jge|jc|jnc|jo|jno|js|jns|jz|jnz)\s+[0-9a-f]+ <([^+>]*)>')
 }
 
 class Relations:
     def __init__(self):
         # index 0 for incoming, 1 for outgoing
+        # a peer is a dictionary whose key is the related symbol, and value is a list of unique relation instructions
         for relation in available_relations.keys():
-            setattr(self, relation, (set(), set()))
+            setattr(self, relation, (dict(), dict()))
 
     def peers(self, relation, forward):
         return getattr(self, relation)[forward]
 
-    def add_relation(self, other, relation, forward):
-        self.peers(relation, forward).add(other)
+    def add_relation(self, other, relation, forward, inst_index):
+        peers = self.peers(relation, forward)
+        if other in peers:
+            if inst_index not in peers[other]:
+                peers[other].append(inst_index)
+        else:
+            peers[other] = [inst_index]
 
 class SymbolRelations:
     def __init__(self):
         self.dict = {}
-        self.abi_version = "v0.0"
+        self.instructions = list()
+        self.abi_version = "v0.1"
 
     def get(self, sym):
         if sym not in self.dict:
             self.dict[sym] = Relations()
         return self.dict[sym]
 
-    def register_relation(self, src, dst, rel):
-        self.get(src).add_relation(dst, rel, forward = True)
-        self.get(dst).add_relation(src, rel, forward = False)
+    def register_relation(self, src, dst, rel, inst):
+        if inst not in self.instructions:
+            self.instructions.append(inst)
+        self.get(src).add_relation(dst, rel, True, self.instructions.index(inst))
+        self.get(dst).add_relation(src, rel, False, self.instructions.index(inst))
 
     def __find_peers_recur(self, found, src_sym, relation, forward, search, recur):
         for sym in self.get(src_sym).peers(relation, forward):
@@ -81,9 +90,10 @@ class SymbolRelations:
                 if not relation:
                     continue
                 # a match of relation is found
-                dst_sym = match.groups()[0]
+                instruction = match.groups()[0]
+                dst_sym = match.groups()[1]
                 if src_sym:
-                    self.register_relation(src_sym, dst_sym, relation)
+                    self.register_relation(src_sym, dst_sym, relation, instruction)
                 else:
                     print("Error matching relation", relation, "[None] ->", dst_sym, "without a matching caller", file=sys.stderr);
                     exit(1)
